0bedd34f6d69313038bec97abe7d3970b7b35280
Emily Babin <W0428981@nscc.ca>
2025-02-19 14:19:21 -0400

    Initial Commit

---
+++ EPath-v0.0.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EPath-v0.0.1
+//
+//  Created by Emily Babin on 2025-02-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EPath-v0.0.1Tests/EPath_v0_0_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EPath_v0_0_1Tests.swift
+//  EPath-v0.0.1Tests
+//
+//  Created by Emily Babin on 2025-02-19.
+//
+
+import Testing
+@testable import EPath_v0_0_1
+
+struct EPath_v0_0_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
