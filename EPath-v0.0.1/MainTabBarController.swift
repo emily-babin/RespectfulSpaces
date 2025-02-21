eb0e25474c51ce1180064071690172e383843027
Emily Babin <W0428981@nscc.ca>
2025-02-21 12:06:33 -0400

    Created TabBarController for Home and Scenarios

---
+++ EPath-v0.0.1/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarController.swift
+//  EPath-v0.0.1
+//
+//  Created by Emily Babin on 2025-02-21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.unselectedItemTintColor = .black
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
