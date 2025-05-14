db8e8270eb525c4a4c31f85210013a7f2337bc94
Carlos Andre Pillas <andreyupillas@gmail.com>
2025-05-14 14:07:10 -0300

    baseViewControllerUpdates
    baseviewcontroller now initializes searchController, navBar and TabBar
    
    viewDidAppear is used to make the text in searchbar white
    
    viewDidDisappear so that when switching tabs the search bar deactivates and clears text
    
    3 setup
    setupTable
    setupNavBar
    setupTabBar
    
    transferred the functions from both scenarios and toolbox to base

--- EPath-v0.0.1/ScenariosViewController.swift
+++ EPath-v0.0.1/ScenariosViewController.swift
@@ -10,8 +10,7 @@ import FirebaseFirestore
 import CoreData
 import Network
 
-class ScenariosViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
-
+class ScenariosViewController: BaseViewController , UITableViewDelegate, UITableViewDataSource {
         
     @IBOutlet weak var table: UITableView!
     
@@ -19,81 +18,28 @@ class ScenariosViewController: UIViewController , UITableViewDelegate, UITableVi
     var repository = FirebaseRepository.shared
     var selectedItem = Scenarios()
     var filteredScenarios: [Scenarios] = []
-    
-    var searchController = UISearchController()
-    var styledSearchBar = false
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupTable()
-        setupNavBar()
-        setupTabBar()
+        
+        //Find this function in BaseViewController File
+        //Setup table design, datasource, delegate
+        setupTable(table: table, dataSource: self, delegate: self)
+        
+        //Adds the data for scenarios
+        setupTableData()
     }
     
-    func setupTable() {
-        table.dataSource = self
-        table.delegate = self
-        table.separatorStyle = .none
-        table.showsVerticalScrollIndicator = false
-        table.keyboardDismissMode = .onDrag
-        
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+
+    func setupTableData() {
         self.filteredScenarios = repository.scenarios
         self.table.reloadData()
     }
-    override func viewDidAppear(_ animated: Bool) {
-        
-        super.viewDidAppear(animated)
-
-        if(!styledSearchBar) {
-            searchController.searchBar.searchTextField.textColor = .white
-            styledSearchBar = true
-        }
-    }
     
-    func initSearchController() {
-        //searchResultsController: nil means the search results will be shown in the same view controller, not a separate one.
-        searchController = UISearchController(searchResultsController: nil)
-        searchController.obscuresBackgroundDuringPresentation = false
-        
-        if let searchBarTextField = searchController.searchBar.value(forKey: "searchField") as? UITextField {
-                
-            searchBarTextField.backgroundColor = UIColor.white.withAlphaComponent(0.10)
-            searchBarTextField.layer.cornerRadius = 8
-            searchBarTextField.clipsToBounds = true
-            
-            let glassIconView = searchBarTextField.leftView as! UIImageView
-            glassIconView.image = glassIconView.image?.withRenderingMode(.alwaysTemplate)
-            glassIconView.tintColor = .white
-            
-            // Set placeholder color
-            searchBarTextField.attributedPlaceholder = NSAttributedString(
-                string: "Search",
-                attributes: [NSAttributedString.Key.foregroundColor: UIColor.white]
-            )
-        }
-
-       
-        //WHITE TINT FOR CANCEL
-        searchController.searchBar.tintColor = .white
-        
-        //Assigns the current view controller (self) as the object that responds to search updates.
-        searchController.searchResultsUpdater = self
-        
-        //Places the search bar in the navigation bar of the view controller.
-        navigationItem.searchController = searchController
-        
-        //This line keeps the search bar always visible
-        navigationItem.hidesSearchBarWhenScrolling = false
-       
-        // Assign the search controller to the navigation item
-        navigationItem.searchController = searchController
-       
-        // Ensure the context is defined to prevent weird UI behavior
-        definesPresentationContext = true
-        
-    }
-  
-    func updateSearchResults(for searchController: UISearchController) {
+    override func updateSearchResults(for searchController: UISearchController) {
 
         let searchText = searchController.searchBar.text ?? ""
         
@@ -113,6 +59,22 @@ class ScenariosViewController: UIViewController , UITableViewDelegate, UITableVi
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            
+        selectedScenario = indexPath.section
+        self.performSegue(withIdentifier: "select_Scenario_Segue", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if (segue.identifier == "select_Scenario_Segue") {
+            
+            let ScenariosDetailVC = segue.destination as! ScenariosDetailViewController
+            
+            ScenariosDetailVC.current_Scenario = filteredScenarios[selectedScenario]
+        }
+    }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
@@ -129,8 +91,6 @@ class ScenariosViewController: UIViewController , UITableViewDelegate, UITableVi
   
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        //Returns how many items in the array which for now is 5
         return 1
     }
     
@@ -148,75 +108,5 @@ class ScenariosViewController: UIViewController , UITableViewDelegate, UITableVi
         cell.iconImageView.image = UIImage(named: scenarios.imageName)
         
         return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
-        selectedScenario = indexPath.section
-        self.performSegue(withIdentifier: "select_Scenario_Segue", sender: self)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        if (segue.identifier == "select_Scenario_Segue") {
-            
-            let ScenariosDetailVC = segue.destination as! ScenariosDetailViewController
-            
-            ScenariosDetailVC.current_Scenario = filteredScenarios[selectedScenario]
-        }
-    }
-    
-    
-    func setupNavBar() {
-        //Fix Navigation Bar Color Change Issue
-        //Initialize a Appearance object which will hold all the design changes for the navBar
-        let navBarAppearance = UINavigationBarAppearance()
-        
-        //Custom RGB color for BUILD NS
-        navBarAppearance.backgroundColor = UIColor(red: 222/255, green: 61/255, blue: 38/255, alpha: 1.0)
-        
-        //Set title text color
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        
-        navigationController?.navigationBar.standardAppearance = navBarAppearance
-        
-        //This will ensure the color of the nav bar above does not change when scrolling
-        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-        
-        //This changes the color of the back button up in navigationw
-        navigationController?.navigationBar.tintColor = UIColor.white
-        
-        initSearchController()
-    }
-
-    func setupTabBar() {
-        //Fix Tab Bar Color Change Issue
-        let tabBarAppearance = UITabBarAppearance()
-       
-        //Same RGB color for consistency
-        tabBarAppearance.backgroundColor = UIColor(red: 221/255, green: 64/255, blue: 38/255, alpha: 1.0)
-       
-        //Create UITabBarItemAppearance to customize the item appearance
-        let itemAppearance = UITabBarItemAppearance()
-
-        //Change the unselected item color
-        itemAppearance.normal.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white] // For normal (unselected) text
-        itemAppearance.normal.iconColor = UIColor.white // For normal (unselected) icon
-
-        //Change the selected item color
-        //For selected text
-        itemAppearance.selected.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        itemAppearance.selected.iconColor = UIColor.white // For selected icon*/
-
-        //Apply the itemAppearance to the standard appearance
-        tabBarAppearance.stackedLayoutAppearance = itemAppearance
-        tabBarAppearance.inlineLayoutAppearance = itemAppearance
-        tabBarAppearance.compactInlineLayoutAppearance = itemAppearance
-
-        //This adds all the changes above
-        tabBarController?.tabBar.standardAppearance = tabBarAppearance
-       
-        //This will ensure the color of the tab bar does not change when scrolling
-        tabBarController?.tabBar.scrollEdgeAppearance = tabBarAppearance
     }
 }
